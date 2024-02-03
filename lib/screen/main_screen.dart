c567a9f370660f7a20cf04e413e8c016c49fc755
jomeullee <jomeullee@gmail.com>
2024-02-03 15:39:30 +0900

    Practice : column, row, expanded

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -11,7 +11,31 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-        child: Text('메인 화면 입니다'),),);
+      appBar: AppBar(
+        title: Text('메인화면'),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('반갑습니다'),
+          Text('저는'),
+          Text('누구입니다'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('어떻게 된다는거야'),
+              Text('row로는 어떻게 쌓이는건지'),
+              Text('궁금하다'),
+            ],
+          ),
+          Row(children: [
+            Expanded(flex:2, child: Text('expanded실험이야')),
+            Expanded(child: Text('이건또뭘까')),
+            Expanded(child: Text('궁금하네')),
+          ],)
+        ],
+      ),
+    );
   }
 }
