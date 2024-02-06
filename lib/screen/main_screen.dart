e66c3e419042b992886c70eaa5bd648c670be614
jomeullee <jomeullee@gmail.com>
2024-02-06 12:17:17 +0900

    Practice : ListView

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -8,29 +8,22 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List listHello = ['안녕', '반가워', '잘가'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('메인화면'),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 100,
-            width: 200,
-            margin: EdgeInsets.all(30),
-            child: ElevatedButton(
-                onPressed: () {
-                  print('버튼이 눌렸습니다');
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.amberAccent,
-                onPrimary: Colors.red,
-                elevation: 40),
-                child: Text('눌러보세요')),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('${listHello[index]}'),
+            subtitle: Text('메롱'),
+          );
+        },
+        itemCount: listHello.length,
       ),
     );
   }
